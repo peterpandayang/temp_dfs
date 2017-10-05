d1cd40e804435c506b616469e5e74a64de2f23b2
peterpandayang <byang14@dons.usfca.edu>
2017-10-05 16:45:01 -0700

    rollback for testing

--- src/main/java/edu/usfca/cs/cache/GeneralCache.java
+++ src/main/java/edu/usfca/cs/cache/GeneralCache.java
@@ -4,7 +4,8 @@ package edu.usfca.cs.cache;
  * Created by bingkunyang on 9/24/17.
  */
 public class GeneralCache {
-    public static final String SERVER_HOSTNAME = "localhost";
-    public static final int SERVER_PORT = 5678;
+    //    public static final String SERVER_HOSTNAME = "localhost";
+    public static final String SERVER_HOSTNAME = "bass01.cs.usfca.edu";
+    public static final int SERVER_PORT = 38000;
     public static final int DEFAULT_REPLICAS = 3;
 }
