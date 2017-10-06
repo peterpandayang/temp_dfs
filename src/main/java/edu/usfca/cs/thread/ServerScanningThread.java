1ca5c93ecea1fcb9274085a79a145a4617822c98
peterpandayang <byang14@dons.usfca.edu>
2017-10-05 22:57:19 -0700

    fix for some build error

--- src/main/java/edu/usfca/cs/thread/ServerScanningThread.java
+++ src/main/java/edu/usfca/cs/thread/ServerScanningThread.java
@@ -3,6 +3,8 @@ package edu.usfca.cs.thread;
 
 import edu.usfca.cs.route.ReplicaMaintainer;
 
+import java.io.IOException;
+
 /**
  * Created by bingkunyang on 10/3/17.
  */
@@ -19,6 +21,8 @@ public class ServerScanningThread extends Thread{
         try {
             maintainer.scanAndFix();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
