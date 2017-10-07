474541fa790b2d4fba2ad6697219fdff1601472c
peterpandayang <byang14@dons.usfca.edu>
2017-10-07 10:05:16 -0700

    commit for removing all files

--- src/main/java/edu/usfca/cs/route/DataNodeDataRemover.java
+++ src/main/java/edu/usfca/cs/route/DataNodeDataRemover.java
@@ -3,7 +3,10 @@ package edu.usfca.cs.route;
 import edu.usfca.cs.cache.DataNodeCache;
 import edu.usfca.cs.thread.DataNodeRemoveAllThread;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -26,9 +29,14 @@ public class DataNodeDataRemover {
         threadPool.execute(thread);
     }
 
-    public void removeAllFile(){
+    public void removeAllFile() throws IOException {
         System.out.println("start removing datanode files...");
         // there should start to do the removal part
-
+        String folderPath = cache.pathPrefix;
+        if(Files.exists(Paths.get(folderPath))){
+            Files.delete(Paths.get(folderPath));
+        }
+        System.out.println("all files have been deleted");
+        socket.close();
     }
 }
